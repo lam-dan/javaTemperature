76800230bfd8e37a6586fe4999d9963e201bb9ae
Daniel <danlam@ucdavis.edu>
2021-09-10 15:57:52 -0700

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -13,8 +13,8 @@ public class Temperature {
 			input = terminal.next();
 			System.out.printf("\"%s\" is not a valid temperature.\n", input);
 			System.out.println("Please enter a valid temperature in Celcius:");
-        }
-        celcius = terminal.nextDouble();		
+        	}
+        	celcius = terminal.nextDouble();		
 		fahrenheit = celcius * 9/5  + 32;
 		System.out.println(celcius + " C = " + fahrenheit + " F");	
 	}
